70532b6f4bb04258207b3691b5977c018932e8bb
Jungki <andy960609@gmail.com>
2022-12-17 00:18:48 +0900

    221217 3차

--- 알고리즘/9번문제(핸드폰번호가리기).js
+++ 알고리즘/9번문제(핸드폰번호가리기).js
@@ -3,8 +3,8 @@
 function solution(phone_number){
     let answer = "";
     for(i = 0; i < phone_number.length; i++){
-        if(i < phone_number.length-4){
-            answer =answer + "*";
+        if(i < phone_number.length - 4){
+            answer = answer + "*";
         }else{
             answer = answer + phone_number[i];
         }
@@ -16,11 +16,8 @@ function solution(phone_number){
 //18:26    내가 아는 만큼만 하자 !
 //1안
 
-function hide_numbers(s){
-    var result = "*".repeat(s.length - 4) + s.slice(-4);
-    //함수를 완성해주세요
-  
-    return result;
+function solution(phone_number){
+    return "*".repeat(phone_number.length - 4) + phone_number.slice(-4);  
   }
 
 
